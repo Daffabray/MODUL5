b0125d4eb1a478634d8a8bde326828bf1ae91b9d
Daffabray <90074627+Daffabray@users.noreply.github.com>
2023-05-22 17:47:34 +0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,15 @@
+package javaapplication4;
+
+/**
+ *
+ * @author Daffa
+ */
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
